c6bf1a904d853c76f4e6067ce60910f11f433caa
Zachwitte21 <zwittecardiff022@gmail.com>
2023-03-29 09:33:34 -0400

    Revert "end of task 8"
    This reverts commit 6bf6327a051fb8073f87cc4215da43e6e4ec99db.

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,42 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-const ChangeHolidayAlphabetically: Record<string, string> = {
-    "Holiday: 🎃": "Holiday: 👴🏾",
-    "Holiday: 🎄": "Holiday: 🎃",
-    "Holiday: 👴🏾": "Holiday: ✨",
-    "Holiday: ✨": "Holiday: 🦃",
-    "Holiday: 🦃": "Holiday: 🎄"
-};
-
-const ChangeHolidayYear: Record<string, string> = {
-    "Holiday: 🎃": "Holiday: 🦃",
-    "Holiday: 🦃": "Holiday: 🎄",
-    "Holiday: 👴🏾": "Holiday: 🎃",
-    "Holiday: 🎄": "Holiday: ✨",
-    "Holiday: ✨": "Holiday: 👴🏾"
-};
-
 export function CycleHoliday(): JSX.Element {
-    const [holiday, setHoliday] = useState<string>("Holiday: 🎃");
-
-    function changeByAlpha(): void {
-        const newHoliday = ChangeHolidayAlphabetically[holiday];
-        setHoliday(newHoliday);
-    }
-
-    function changeByYear(): void {
-        const newHoliday = ChangeHolidayYear[holiday];
-        setHoliday(newHoliday);
-    }
-
-    return (
-        <div>
-            <span>
-                <Button onClick={changeByAlpha}>Advance By Alphabet</Button>
-                <Button onClick={changeByYear}>Advance By Year</Button>
-            </span>
-            <div>{holiday}</div>
-        </div>
-    );
+    return <div>Cycle Holiday</div>;
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,30 +12,5 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    const [dice, setDice] = useState<number>(1);
-    const [dice2, setDice2] = useState<number>(6);
-
-    function sameDice(): string {
-        if (dice === dice2) {
-            if (dice === 1) {
-                return "You Lost!";
-            } else {
-                return "You Won!";
-            }
-        }
-        return "";
-    }
-    return (
-        <div>
-            <span>
-                <Button onClick={() => setDice(d6)}>Roll Left</Button>
-                <span data-testid="left-die"> {dice} </span>
-            </span>
-            <span>
-                <Button onClick={() => setDice2(d6)}>Roll Right</Button>
-                <span data-testid="right-die"> {dice2} </span>
-            </span>
-            <div>{sameDice()}</div>
-        </div>
-    );
+    return <div>Two Dice</div>;
 }
